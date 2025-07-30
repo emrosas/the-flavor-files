953c1424f2a1f1e140b6437ab6d21b74753a86b2
Erick Mireles <emireles.rosas@gmail.com>
2025-07-30 12:58:12 -0500

    style: add empty state to preview

--- components/CreateRecipePreview.tsx
+++ components/CreateRecipePreview.tsx
@@ -1,23 +1,62 @@
 import { useCreateRecipeForm } from "./CreateRecipeFormProvider";
 
 export default function CreateRecipePreview() {
-  const { title, description } = useCreateRecipeForm();
   return (
     <div>
-      <div className="px-6 pt-6 pb-5 rounded-lg bg-latte-1 mb-8">
-        <div className="aspect-video bg-brand-3 rounded-md mb-2" />
-        <h2
-          className={`font-medium text-3xl ${title !== "" ? "opacity-100" : "opacity-50"}`}
-        >
-          {title !== "" ? title : "Your Recipe Name"}
-        </h2>
-        <p className={`${description !== "" ? "opacity-100" : "opacity-50"}`}>
-          {description !== ""
-            ? description
-            : "Here goes your recipe descriptiom. Keep it short and sweet (under 120 characters)."}
-        </p>
+      <PreviewCard />
+      <PreviewExtra />
+    </div>
+  );
+}
+
+function PreviewCard() {
+  const { title, description } = useCreateRecipeForm();
+
+  return (
+    <div className="px-6 pt-6 pb-5 rounded-lg bg-latte-1 mb-8">
+      <div className="aspect-video bg-brand-3 rounded-md mb-2" />
+      <h2
+        className={`font-medium text-3xl ${title !== "" ? "opacity-100" : "opacity-50"}`}
+      >
+        {title !== "" ? title : "Your Recipe Name"}
+      </h2>
+      <p className={`${description !== "" ? "opacity-100" : "opacity-50"}`}>
+        {description !== ""
+          ? description
+          : "Here goes your recipe descriptiom. Keep it short and sweet (under 120 characters)."}
+      </p>
+    </div>
+  );
+}
+
+function PreviewExtra() {
+  const { ingredients, instructions } = useCreateRecipeForm();
+  return (
+    <div className="px-6 py-5 rounded-lg bg-latte-1 flex gap-4">
+      <div className="grow">
+        <h3 className="text-xl font-semibold font-serif">Ingredients</h3>
+        {ingredients && ingredients.length > 0 ? (
+          <ul>
+            {ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        ) : (
+          <p className="opacity-50">No ingredients added yet.</p>
+        )}
       </div>
-      <div className="px-6 py-5 rounded-lg bg-latte-1 ">Secondary Card</div>
+      <div className="grow">
+        <h3 className="text-xl font-semibold font-serif">Instructions</h3>
+        {instructions && instructions.length > 0 ? (
+          <ol>
+            {instructions.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+        ) : (
+          <p className="opacity-50">No instructions added yet.</p>
+        )}
+      </div>
     </div>
   );
 }
